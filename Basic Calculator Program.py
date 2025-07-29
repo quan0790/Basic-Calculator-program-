28fd86a2716add70ed0d5aa2b8b2b04223db0a0a
Kibet <103100894+quan0790@users.noreply.github.com>
2025-07-29 19:13:25 +0300

    Update Basic Calculator Program.py

--- Basic Calculator Program.py
+++ Basic Calculator Program.py
@@ -1,11 +1,7 @@
-# Basic Calculator Program
-
-# Get user input
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter an operation (+, -, *, /): ")
 
-# Perform calculation
 if operation == '+':
     result = num1 + num2
     print(f"{num1} + {num2} = {result}")
